d506c16d58c703441c25dab830400259ad794877
MasumKhan <masumkhan081@gmail.com>
2020-12-31 23:19:57 +0600

    Update Binomial_Coefficient.java

--- Number_Theory/Binomial_Coefficient.java
+++ Number_Theory/Binomial_Coefficient.java
@@ -13,32 +13,28 @@ import java.util.Scanner;
  */
 public class Binomial_Coefficient_SpaceOptimized {
 
-              public static void main(String[] args) {
-                            Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
 
-                            while (true) {
-                                          System.out.print("n:  ");
-                                          int n = scn.nextInt();
-                                          System.out.print("r:  ");
-                                          int r = scn.nextInt();
+        while (true) {
+            System.out.print("n:  ");
+            int n = scn.nextInt();
+            System.out.print("k:  ");
+            int k = scn.nextInt();
 
-                                          r = Math.min(n, r);
-                                          n = Math.max(n, r);
+            System.out.println("Fucking Coefficient Is: " + get_binomial_coeff(n, k));
+        }
+    }
 
-                                          int the_Coefficient = get_binomial_coeff(n, r);
-                                          System.out.println("Fucking Coefficient Is: " + the_Coefficient);
-                            }
-              }
+    static int get_binomial_coeff(int n, int k) {
+        int arr[] = new int[k + 1];
+        arr[0] = 1;
 
-              static int get_binomial_coeff(int n, int r) {
-                            int arr[] = new int[r + 1];
-                            arr[0] = 1;
-
-                            for (int i = 1; i <= n; i++) {
-                                          for (int j = Math.min(i, r); j > 0; j--) {
-                                                        arr[j] = arr[j] + arr[j - 1];
-                                          }
-                            }
-                            return arr[r];
-              }
+        for (int i = 1; i <= n; i++) {
+            for (int j = Math.min(i, k); j > 0; j--) {
+                arr[j] = arr[j] + arr[j - 1];
+            }
+        }
+        return arr[k];
+    }
 }
