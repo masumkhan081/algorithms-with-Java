d9a88465be1d1f0085ee60c8d77bc18398f0575e
masum khan <masumkhan081@gmail.com>
2022-03-17 12:01:44 +0600

    Update Binary_Search.java

--- Searching_AND_Sorting/Binary_Search.java
+++ Searching_AND_Sorting/Binary_Search.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Searching_And_Sorting;
-
 import java.util.Scanner;
-
 /**
  *
  * @author Masum Khan
  */
-public class Binary_Search {
+public class Binary_Search { // Uploaded, bug fixed and commented: 17.03.22
 
     public static void main(String[] args) {
 
         int init, end, mid, num_To_Find;
+        // an already sorted array
         int arr_inputs[] = {1, 3, 5, 7, 9, 11, 17, 78, 900, 2344, 4444, 5670, 10000, 10001, 10002, 10003, 10004};
         Scanner scn = new Scanner(System.in);
         while (true) {
-            System.out.println("\nnumber to be found: ");
+            System.out.println("\nNumber to be found? : ");
             num_To_Find = scn.nextInt();
             init = 0;
-            end = arr_inputs.length;
+            // as at some point init, end, and mid can be same, and i'm checking arr[mid] so end has to be len-1 (the bug fixed)
+            end = arr_inputs.length - 1;
             mid = (init + end) / 2;
 
             while (init <= end) {
@@ -37,9 +32,9 @@ public class Binary_Search {
                 mid = (init + end) / 2;
             }
             if (init > end) {
-                System.out.println("No Such Value.");
+                System.out.println("No Such Item.");
             }
         }
-
+        // note: tricky input: value at primary mid index, lowest, highest, bigger than highest , smaller than lowest. 
     }
 }
