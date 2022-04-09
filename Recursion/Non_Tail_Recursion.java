aa30617fc9b2be0d0891ef7901f82ba275a94bf0
masum khan <masumkhan081@gmail.com>
2022-04-09 17:47:44 +0600

    Update Non_Tail_Recursion.java

--- Recursion/Non_Tail_Recursion.java
+++ Recursion/Non_Tail_Recursion.java
@@ -6,16 +6,28 @@ package Recursion;
  */
 public class Non_Tail_Recursion {
 
-    public static void main(String[] args) { // checked and revised at 09 April 22
-        int n = 3;
-        haha(n);
+    public static void main(String[] args) {        // checked and revised at 09 April 22
+        int n;
+
+        example_1(3);
+        // 
+        System.out.println("\n\n Number of division by 2: " + example_2(16));
+        System.out.println("\n\n Number of division by 2: " + example_2(8));
     }
 
-    static void haha(int n) {
+    static int example_2(int n) { // counts how much division by 2 is possile untill n is equal 1.
+        if (n == 1) {
+            return 0;
+        } else {
+            return 1 + example_2(n / 2);  // addition of 1 is after recursive call.
+        }
+    }
+
+    static void example_1(int n) {
         if (n == 0) {
             return;
         }
-        haha(n - 1);  // this function call is not the last thing done by the function itself. 
+        example_1(n - 1);  // this function call is not the last thing done by the function itself. 
         System.out.println("--> " + n);  // here's something to be evaluated after recursive call.
     }
 }
