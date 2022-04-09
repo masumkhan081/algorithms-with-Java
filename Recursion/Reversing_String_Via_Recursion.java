503338df5aeb0cdf9e37c5985dc6b40f4c7faec4
masum khan <masumkhan081@gmail.com>
2022-04-09 16:32:57 +0600

    Update Reversing_String_Via_Recursion.java

--- Recursion/Reversing_String_Via_Recursion.java
+++ Recursion/Reversing_String_Via_Recursion.java
@@ -1,33 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Recursion;
-
 import java.util.Scanner;
-
 /**
  *
  * @author Masum Khan
  */
-public class Reversing_String_Via_Recursion{
+public class Reversing_String_Via_Recursion { // checked and revised at 09 April 22
 
     public static void main(String[] args) {
-
-//      while(true){
-//      Scanner scn = new Scanner(System.in);
-//      String str = scn.next();
-        String str = "Sandy";
+        
+        String str;
+        // .......  TEST CASEs
+        str = "Sandy";
+        str = "abcd";
+        str = "banana";
+        str = "level";
+        //
         String reversed = reverseString(str);
         System.out.println("Reversed string: " + reversed);
-//      }
     }
-
     public static String reverseString(String str) {
-        if (str.isEmpty()) {
+
+        if (str.isEmpty()) {  // base cndition
             return str;
         }
+        /*
+        str keeps shrinking by 1 untill empty and every time 1st char of str is added to the end of the 
+        returned value of per method call. 
+         */
         return reverseString(str.substring(1)) + str.charAt(0);
     }
 }
