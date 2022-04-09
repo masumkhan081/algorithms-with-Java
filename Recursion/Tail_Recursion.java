1ab0543027c3ddcca4d9ff5473242007cbebcff9
masum khan <masumkhan081@gmail.com>
2022-04-09 16:53:38 +0600

    Update Tail_Recursion.java

--- Recursion/Tail_Recursion.java
+++ Recursion/Tail_Recursion.java
@@ -1,28 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Recursion;
-
 /**
  *
  * @author Masum Khan
  */
 public class Tail_Recursion {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // checked and revised at 09 April 22
         int n = 3;
         haha(n);
     }
-
     static int haha(int n) {
         if (n == 0) {
             return 0;
         } else {
-            System.out.println("__" + n);
+            System.out.println("--> " + n);
         }
+      /* as recursive call is the last thing done by the function, so it's called tail recursion
+          it's just an example to stand with the theory of it
+      */
         return haha(n - 1);
     }
-
 }
