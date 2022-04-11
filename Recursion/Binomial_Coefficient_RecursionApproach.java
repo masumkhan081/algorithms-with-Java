65bf41e624faa31245c0748b20dd12479d3a8c64
masum khan <masumkhan081@gmail.com>
2022-04-11 19:16:23 +0600

    Update Binomial_Coefficient_RecursionApproach.java

--- Recursion/Binomial_Coefficient_RecursionApproach.java
+++ Recursion/Binomial_Coefficient_RecursionApproach.java
@@ -1,36 +1,29 @@
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Recursion;
-
 import java.util.*;
 
 /**
  *
  * @author Masum Khan
  */
-public class Binomial_Coefficient_RecursionApproach {
+public class Binomial_Coefficient_RecursionApproach {          // checked and revised at 11 April 22
+// src:   https://youtu.be/R6HHjzElTUM
+    public static void main(String[] args) {
 
-              public static void main(String[] args) {
+        int n = 5, r = 2; 
+       
+        int the_coefficient = get_binomial_coeff(n, r);
+        System.out.println("Binomial Coefficient: " + the_coefficient);
+    }
 
-                            int n = 5, r = 2;
-//                       n=3 ; r=2;          // test case-2
-//                         n= 7; r=3;       //  test case-3
+    static int get_binomial_coeff(int n, int r) {
 
-                            int the_coefficient = get_binomial_coeff(n, r);
-                            System.out.println("Binomial Coefficient: " + the_coefficient);
-              }
-
-              static int get_binomial_coeff(int n, int r) {
-
-                            if (r == 0 || r == n) {
-                                          return 1;
-                            }
-                            return get_binomial_coeff(n - 1, r - 1)
-                                    + get_binomial_coeff(n - 1, r);
-              }
-
+        if (r == 0 || r == n) {
+            return 1;
+        }
+        return get_binomial_coeff(n - 1, r - 1)
+                + get_binomial_coeff(n - 1, r);
+    }
 }
+ /*  TEST CASE
+                 n=3 ; r=2;  
+                 n= 7; r=3; 
+*/
