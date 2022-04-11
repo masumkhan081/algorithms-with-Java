1b443c78f926a6aa32cbc1b52855c1c18200f35e
masum khan <masumkhan081@gmail.com>
2022-04-11 20:10:09 +0600

    Update Binary_Search_Recursive_Way.java

--- Recursion/Binary_Search_Recursive_Way.java
+++ Recursion/Binary_Search_Recursive_Way.java
@@ -1,31 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Recursion;
-
 import java.util.Scanner;
 
 /**
  *
  * @author Masum Khan
  */
-public class Binary_Search_Recursive_Way {
+public class Binary_Search_Recursive_Way {           // checked and revised at 11 April 
 
     public static void main(String[] args) {
 
         int arr_inputs[] = {1, 3, 5, 7, 9, 11, 17, 78, 900, 2344, 4444, 5670, 10000, 10001, 10002, 10003, 10004};
+
         Scanner scn = new Scanner(System.in);
         int len = arr_inputs.length;
         while (true) {
-            System.out.println("number to be found: ");
+            System.out.print("number to be found:  ");
             int num_To_Find = scn.nextInt();
             int the_Index = finder(arr_inputs, 0, len - 1, num_To_Find);
             if (the_Index == -1) {
                 System.out.println("No Such Element ");
             } else {
-                System.out.println("Index: " + the_Index + "\n\n");
+                System.out.print("Index: " + the_Index + "\n\n");
             }
         }
     }
@@ -39,8 +33,9 @@ public class Binary_Search_Recursive_Way {
             if (arr[mid] > x) {
 
                 return finder(arr, l, mid - 1, x);
+            } else {
+                return finder(arr, mid + 1, r, x);
             }
-            return finder(arr, mid + 1, r, x);
         }
         return -1;
     }
