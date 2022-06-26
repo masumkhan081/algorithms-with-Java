b0841f732364cafb90d3104ee9a2e69b4d81d91b
masum khan <masumkhan081@gmail.com>
2022-06-26 20:16:02 +0600

    Update Z_algorithm.java

--- Pattern_Searching/Z_algorithm.java
+++ Pattern_Searching/Z_algorithm.java
@@ -1,24 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Pattern_Matching;
-
 /**
  *
  * @author Masum Khan
  */
-public class Z_algorithm {
+public class Z_algorithm {   //   Uploaded, bug checked and commented: 26.06.22
 
     static String text = "xabcabzabc";
     static String pattern = "abc";
 
-    public static void main(String[] args) {  //   Study Source:     https://youtu.be/CpZh4eF8QBw
+    public static void main(String[] args) {   //  Study Source:     https://youtu.be/CpZh4eF8QBw
 
         int i, j, count;
-        // adding special char between pattern + text .
-        text = pattern + "$" + text;
+
+        text = pattern + "$" + text; // adding special char between pattern + text .
+
+        //text = "aabxaayaab" + '$' + "aab";   // alternative test case-1
 
         int z_arr[] = new int[text.length()];
         int len = text.length();
@@ -30,18 +26,16 @@ public class Z_algorithm {
             while (text.charAt(i + j) == text.charAt(j)) {
                 count++;
                 j++;
-                if (i + j == len) {
+                if (i + j == len) { // checking till last index, not more than that ofcourse
                     break;
                 }
             }
             z_arr[i] = count;
         }
-        for (i = pattern.length() + 1; i < z_arr.length; i++) {
+        for (i = pattern.length() + 1; i < z_arr.length; i++) { // search for where the value equal to pattern length lies
             if (z_arr[i] == pattern.length()) {
                 System.out.println("Pattern Exist At : " + (i - (pattern.length() + 1)));
             }
         }
-
     }
-
 }
