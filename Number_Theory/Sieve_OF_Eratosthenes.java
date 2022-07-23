7ead3e088acbeaf1e5c5ff601640acdee5b4c0a6
masum khan <masumkhan081@gmail.com>
2022-07-23 20:15:23 +0600

    Create Sieve_OF_Eratosthenes.java

---
+++ Number_Theory/Sieve_OF_Eratosthenes.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Masum Khan
+ */
+public class Sieve_OF_Eratosthenes {      // checked: 23.07.22
+
+    /*  
+        104729 is the 10000th prime
+    	7919 is 1000 th prime
+        541 is 100th prime
+     */
+    public static void main(String[] args) {
+
+        int n, i, j;
+        Scanner scn = new Scanner(System.in);
+        while (true) {
+            System.out.println("#. prime numbers untill 'n' to be listed"); // .
+            System.out.print("n=? : ");
+            n = scn.nextInt();
+
+            boolean arr_bool[] = new boolean[n + 1];
+
+            for (i = 2; i * i <= n; i++) {
+                if (arr_bool[i] == false) {
+                    for (j = i * i; j <= n; j += i) {
+                        arr_bool[j] = true;
+                    }
+                }
+            }
+            int count = 1;
+            for (i = 2; i <= n; i++) {
+                if (arr_bool[i] == false) {
+                    System.out.println(count + " th:  " + i);
+                    count++;
+                }
+            }
+        }
+    }
+}
